3121b2461965a0091ce4a16686add3a8749e4cc6
Jamie Wade <jamiewade94@icloud.com>
2021-05-08 21:43:41 +0100

    Working and ready to start adding code back

--- PieBot.py
+++ PieBot.py
@@ -1,3 +1,21 @@
 from functions import *
+import schedule
 
 pre_flight_checks()
+
+
+def piebot(pairs):
+    print("Bot working")
+    for pair in pairs:
+        print(pair[0])
+
+
+if ENVIRONMENT == "production":
+    schedule.every().hour.at(":00").do(piebot, pairs=pair_list)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+else:
+    piebot(pairs=pair_list)
