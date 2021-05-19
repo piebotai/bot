4715670a3a1fbfae6e1f13a41fc56636107441e8
Jamie Wade <jamiewade94@icloud.com>
2021-05-19 17:33:22 +0100

    Adjusts example coin pairs for Binance

--- _config-example.py
+++ _config-example.py
@@ -4,16 +4,16 @@ api_secret = "xxx"
 
 # The list of coin pairs you want to trade with
 pair_list = [
-    ("ADA", "ADA_USDT"),
-    ("ALGO", "ALGO_USDT"),
-    ("ATOM", "ATOM_USDT"),
-    ("BTC", "BTC_USDT"),
-    ("CRO", "CRO_USDT"),
-    ("DOT", "DOT_USDT"),
-    ("ETH", "ETH_USDT"),
-    ("LTC", "LTC_USDT"),
-    ("XLM", "XLM_USDT"),
-    ("XRP", "XRP_USDT")
+    ("ADA", "ADAUSDT"),
+    ("ALGO", "ALGOUSDT"),
+    ("ATOM", "ATOMUSDT"),
+    ("BNB", "BNBUSDT"),
+    ("BTC", "BTCUSDT"),
+    ("DOT", "DOTUSDT"),
+    ("ETH", "ETHUSDT"),
+    ("LTC", "LTCUSDT"),
+    ("XLM", "XLMUSDT"),
+    ("XRP", "XRPUSDT")
 ]
 
 # How much USDT do you want to keep as a reserve. This is a percentage of the total portfolio balance
