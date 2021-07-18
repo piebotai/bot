da273387a7e60846e59b460ac65fa7528f4a16da
Jamie Wade <jamiewade94@icloud.com>
2021-07-18 12:35:01 +0100

    Starting console output

--- PieBot.py
+++ PieBot.py
@@ -4,8 +4,28 @@ import schedule
 
 pre_flight_checks()
 
+
+# Buy more coins at a regular interval
+def buy(pairs):
+    # Let users know the bot has been called and is running
+    print()
+    print(colored("Buy", "yellow"))
+    print(emoji.emojize(":mag:", use_aliases=True), end=" ")
+    print(colored("Collecting current balances", "cyan"))
+
+
+# Rebalance all coin pairs so they are on target
+def rebalance(pairs):
+    # Let users know the bot has been called and is running
+    print()
+    print(colored("Rebalance", "yellow"))
+    print(emoji.emojize(":mag:", use_aliases=True), end=" ")
+    print(colored("Collecting current balances", "cyan"))
+
+
 if environment == "production":
     print("Production")
 
 else:
-    print("Dev")
+    buy(pairs=pair_list)
+    rebalance(pairs=pair_list)
