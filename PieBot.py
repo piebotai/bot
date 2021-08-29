5134395484e77b2ded959acfffe0529dee099c9b
Jamie Wade <jamiewade94@icloud.com>
2021-08-29 07:48:57 +0100

    Performance improvements (#36)
    * Stores Rebalance order data in a 2D list, rather than using more API calls
    
    * Adjusts timeout times
    
    * Adds garbage collection after each task runs

--- PieBot.py
+++ PieBot.py
@@ -1,4 +1,5 @@
 from functions import *
+import gc
 import schedule
 import signal
 
@@ -51,6 +52,8 @@ def buy(pairs):
     else:
         print(colored("Not enough USDT available", "yellow"))
 
+    gc.collect()
+
     print(colored("Waiting to be called...", "cyan"))
 
 
@@ -61,28 +64,26 @@ def rebalance(pairs):
     print(colored("Rebalance", "yellow"))
     print(colored("Placing orders...", "cyan"))
 
-    total_portfolio_value = get_portfolio_value(pairs, False)
+    order_data = []
+    total_portfolio_value = 0
+
+    for pair in pairs:
+        coin_balance = get_coin_balance(pair[0])
+        coin_price = get_coin_price(pair[1])
+        pair_value = coin_balance * coin_price
+
+        order_data.append([pair[0], pair[1], coin_price, pair_value])
+        total_portfolio_value += pair_value
 
     # Equally divide the balance by the number of coins, so we know the target value each coin should aim for
     target_per_coin = total_portfolio_value / len(pairs)
 
-    buy_orders = []
-    sell_orders = []
-    total_orders = 0
+    buy_orders_data = []
+    sell_orders_data = []
 
-    for pair in pairs:
-        # Sets null defaults
-        difference = 0
-        order_value = 0
-        pair_value = 0
-
-        # Gets the total number of coins for this coin
-        coin_balance = get_coin_balance(pair[0])
-
-        # Gets the current price for this coin
-        coin_price = get_coin_price(pair[1])
-
-        pair_value = coin_balance * coin_price
+    for pair in order_data:
+        coin_price = pair[2]
+        pair_value = pair[3]
 
         # If the coin value is over target, sell the excess if it's difference is greater than or equal to the minimum order value
         if pair_value > target_per_coin:
@@ -90,7 +91,7 @@ def rebalance(pairs):
 
             if difference >= min_order_value:
                 order_value = difference / coin_price
-                sell_orders.append([pair[0], pair[1], order_value, difference])
+                sell_orders_data.append([pair[0], pair[1], order_value, difference])
 
         # If the coin value is under target, buy more if it's difference is greater than or equal to the minimum order value
         elif pair_value < target_per_coin:
@@ -98,14 +99,14 @@ def rebalance(pairs):
 
             if difference >= min_order_value:
                 order_value = difference
-                buy_orders.append([pair[0], pair[1], order_value, difference])
+                buy_orders_data.append([pair[0], pair[1], order_value, difference])
 
-    if len(sell_orders) >= 1:
-        for order in sell_orders:
+    if len(sell_orders_data) >= 1:
+        for order in sell_orders_data:
             if environment == "production":
                 order_confirmed = False
                 order_request = order_sell(order[1], order[2])
-                time.sleep(0.25)
+                time.sleep(0.1)
                 if order_request.status_code == 200:
                     order_confirmed = True
 
@@ -124,12 +125,12 @@ def rebalance(pairs):
                 print(str(print_value) + " USDT - " + order[0], end=" ")
                 print(colored("[SELL]", "magenta"))
 
-    if len(buy_orders) >= 1:
-        for order in buy_orders:
+    if len(buy_orders_data) >= 1:
+        for order in buy_orders_data:
             if environment == "production":
                 order_confirmed = False
                 order_request = order_buy(order[1], order[2])
-                time.sleep(0.25)
+                time.sleep(0.1)
                 if order_request.status_code == 200:
                     order_confirmed = True
 
@@ -148,10 +149,15 @@ def rebalance(pairs):
                 print(str(print_value) + " USDT - " + order[0], end=" ")
                 print(colored("[BUY]", "green"))
 
-    total_orders = len(sell_orders) + len(buy_orders)
+    total_orders = len(sell_orders_data) + len(buy_orders_data)
     if total_orders == 0:
         current_time(True)
         print(colored("No coins were eligible to be rebalanced", "yellow"))
+
+    del order_data
+    del buy_orders_data
+    del sell_orders_data
+    gc.collect()
 
     print(colored("Waiting to be called...", "cyan"))
 
